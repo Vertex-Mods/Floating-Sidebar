5d01d6173c8f9950aef54e125ca90abcadf369ff
BibekBhusal0 <BibekBhusal0@users.noreply.github.com>
2026-01-09 11:37:51 +0000

    Update to v1.1.1b

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
+> [!WARNING]
+> This is a beta version and may contain issues. Some bugs and breaking changes are expected.
+
 <h1 align="center">Floating Sidebar</h1>
 <div align="center">
     <a href="https://zen-browser.app/">
--- floating-sidebar.uc.js
+++ floating-sidebar.uc.js
@@ -2,8 +2,8 @@
 // @name            Floating Sidebar
 // @description     Lightweight script to, make firefox Sidebar Floating also option to pin/unpin the sidebar.
 // @author          Bibek Bhusal
-// @version         1.1.0
-// @lastUpdated     2026-01-04
+// @version         1.1.1b
+// @lastUpdated     2026-01-09
 // @ignorecache
 // @homepage        https://github.com/Vertex-Mods/Floating-Sidebar
 // ==/UserScript==
@@ -19,8 +19,8 @@
 })((function () { 'use strict';
 
   function startupFinish(callback) {
-    if (typeof UC_API === "undefined") return;
-    UC_API.Runtime.startupFinished().then(() => callback());
+    if (document.readyState === "complete") callback();
+    else window.addEventListener("load", callback, { once: true });
   }
 
   const parseElement = (elementString, type = "html") => {
@@ -52,20 +52,63 @@
     pin: `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path fill="none" stroke="context-fill light-dark(black, white)" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 17v5M9 10.76a2 2 0 0 1-1.11 1.79l-1.78.9A2 2 0 0 0 5 15.24V16a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1v-.76a2 2 0 0 0-1.11-1.79l-1.78-.9A2 2 0 0 1 15 10.76V7a1 1 0 0 1 1-1a2 2 0 0 0 0-4H8a2 2 0 0 0 0 4a1 1 0 0 1 1 1z"/></svg>`,
     unpin: `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path fill="none" stroke="context-fill light-dark(black, white)" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 17v5m3-12.66V7a1 1 0 0 1 1-1a2 2 0 0 0 0-4H7.89M2 2l20 20M9 9v1.76a2 2 0 0 1-1.11 1.79l-1.78.9A2 2 0 0 0 5 15.24V16a1 1 0 0 0 1 1h11"/></svg>`};
 
+  function setPref(key, value) {
+    try {
+      const prefService = Services.prefs;
+      if (typeof value === "boolean") {
+        prefService.setBoolPref(key, value);
+      } else if (typeof value === "number") {
+        prefService.setIntPref(key, value);
+      } else {
+        prefService.setStringPref(key, value);
+      }
+    } catch {
+      //ignore
+    }
+  }
+
+  const getPref = (key, defaultValue) => {
+    try {
+      const prefService = Services.prefs;
+      if (prefService.prefHasUserValue(key)) {
+        switch (prefService.getPrefType(key)) {
+          case prefService.PREF_STRING:
+            return prefService.getStringPref(key);
+          case prefService.PREF_INT:
+            return prefService.getIntPref(key);
+          case prefService.PREF_BOOL:
+            return prefService.getBoolPref(key);
+        }
+      }
+    } catch (e) {
+      return defaultValue;
+    }
+    return defaultValue;
+  };
+
+  function addPrefListener(name, callback) {
+    const modified_callback = () => {
+      callback({ value: getPref(name) });
+    };
+    Services.prefs.addObserver(name, modified_callback);
+    return { name, callback };
+  }
+
   function addButton() {
     const header = document.getElementById("sidebar-header");
 
     if (!header) return;
     const button = parseElement(`<toolbarbutton id="sidebar-pin-unpin"/>`, "xul");
-    const pref = UC_API.Prefs.get("extension.sidebar-float");
+    const PREF_KEY = "extension.sidebar-float";
+    const floating = () => getPref(PREF_KEY, false);
     function updateImage() {
-      const icon = pref.value ? icons["pin"] : icons["unpin"];
+      const icon = floating() ? icons["pin"] : icons["unpin"];
       button.setAttribute("image", escapeXmlAttribute(svgToUrl(icon)));
     }
     updateImage();
-    pref.addListener(updateImage);
+    addPrefListener(PREF_KEY, updateImage);
 
-    const buttonClick = () => pref.setTo(!pref.value);
+    const buttonClick = () => setPref(PREF_KEY, !floating());
 
     button.addEventListener("click", buttonClick);
     const children = header.children;
